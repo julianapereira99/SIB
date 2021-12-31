005bd8f357b51a26fc9e32f6a2bd08b45458477e
julianapereira99 <juliana.pereira05.99@gmail.com>
2021-12-31 02:31:40 +0000

    files updates

--- src/si/supervised/__init__.py
+++ src/si/supervised/__init__.py
@@ -3,5 +3,5 @@ from .knn import Knn
 from .naive_bayes import GaussianNB
 from .linreg import LinearRegression, LinearRegressionReg, LogisticRegression, LogisticRegressionReg
 from .dt import DecisionTree, Node
-from .Ensemble import Ensemble, average, majority, ConfusionMatrix
+from .ensemble import Ensemble, average, majority, ConfusionMatrix
 from .nn import *
